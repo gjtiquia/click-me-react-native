f631b5c31c6376f6ae9c8c98cc9a095db61662f6
GJ Tiquia <gershom.tiquia@gmail.com>
2023-03-23 00:14:30 +0800

    styled navbar; hide status bar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import ClickMeButton from './components/ClickMeButton';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { GestureResponderEvent, Text, View } from 'react-native';
+import * as NavigationBar from 'expo-navigation-bar';
 
 export default function App() {
   const [count, setCount] = useState(0);
+
+  // Init Android NavBar
+  useEffect(() => {
+    NavigationBar.setBorderColorAsync("transparent");
+    NavigationBar.setBackgroundColorAsync("rgb(30,41,59)") // Same as bg-slate-800
+  }, []);
 
   const onButtonPress = (event: GestureResponderEvent) => {
     setCount(currentCount => currentCount + 1);
@@ -12,7 +19,8 @@ export default function App() {
 
   return (
     <View className='flex-1 items-center justify-center bg-slate-800 gap-8'>
-      <StatusBar style="auto" />
+      <StatusBar style={"auto"} hidden={true} />
+
       <Text className='text-4xl text-slate-50 font-bold'>Click Me</Text>
       <Text className='text-8xl text-slate-50 font-bold'>{count}</Text>
       <View>
